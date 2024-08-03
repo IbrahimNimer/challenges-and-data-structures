1d1767add41ddaa3caff38cea7561a52b0a0bc25
Ibrahim Nimer <23039958@student.ltuc.com>
2024-08-03 17:32:42 +0300

    DMES

---
+++ Challenges/Data Structures/Stack & Queue/StackAndQueue/StackAndQueue/DeleteMiddleElement/README.md
@@ -0,0 +1,19 @@
+﻿## StackWithDeleteMiddle
+
+
+The `DeleteMiddle` method in the `StackWithDeleteMiddle` class effectively removes the middle element from a given stack. It operates by:
+
+* **Calculating the middle index:** Determines the position of the middle element based on the stack's size.
+* **Transferring elements:** Temporarily moves elements from the original stack to an auxiliary stack until the middle element is reached.
+* **Deleting the middle:** Removes the middle element from the original stack.
+* **Restoring the stack:** Transfers elements back from the auxiliary stack to the original stack, preserving the original order of the remaining elements.
+
+The method handles scenarios where the stack is empty or contains an odd number of elements.
+
+## Whiteboard Diagram
+
+![ReverseStackUsingQueue](assets/StackDeletionWB.PNG)
+
+## The Real Code
+
+![The Real Code](assets/StackDeletionCode.PNG)
---
+++ Challenges/Data Structures/Stack & Queue/StackAndQueue/StackAndQueue/DeleteMiddleElement/StackWithDeleteMiddle.cs
@@ -0,0 +1,56 @@
+﻿using StackAndQueue.Stack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue.DeleteMiddleElement
+{
+    public class StackWithDeleteMiddle
+    {
+        public void DeleteMiddle(StackSolution stack)
+        {
+            if (stack.IsEmpty())
+            {
+                Console.WriteLine("Stack is empty. No middle element to delete.");
+                return;
+            }
+
+            int n = 0;
+            StackSolution stacksaver1 = new StackSolution();
+
+            while (!stack.IsEmpty())
+            {
+                stacksaver1.Push(stack.Pop());
+
+                n++;
+
+            }
+
+            while (!stacksaver1.IsEmpty())
+            {
+                stack.Push(stacksaver1.Pop());
+            }
+
+          
+            int middleElement = (n % 2 == 0) ? (n / 2) - 1 : n / 2;
+
+
+            StackSolution stacksaver2 = new StackSolution();
+
+            for (int i = 0; i < middleElement; i++)
+            {
+                stacksaver2.Push(stack.Pop());
+
+            }
+            stack.Pop();
+            while (!stacksaver2.IsEmpty())
+            {
+                stack.Push(stacksaver2.Pop());
+            }
+
+
+        }
+    }
+}
